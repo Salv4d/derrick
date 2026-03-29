0befdf03bb607ecc24f310e5f4eca51c1a98da35
Salvador Oliveira <slsalvadorlucas@gmail.com>
2026-03-29 15:54:03 -0400

    feat: add shell command

---
+++ cmd/derrick/shell.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Salv4d/derrick/internal/engine"
+	"github.com/Salv4d/derrick/internal/ui"
+	"github.com/spf13/cobra"
+)
+
+var shellCmd = &cobra.Command{
+	Use: "shell",
+	Short: "Drop into the isoled Nix development sandbox",
+	Run: func(cmd *cobra.Command, args []string) {
+		cwd, _ := os.Getwd()
+		ui.Infof("Opening sandbox at %s", cwd)
+
+		eng := engine.NewShellEngine()
+		if err := eng.EnterSandbox(cwd); err != nil {
+			ui.FailFast(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(shellCmd)
+}
---
+++ internal/engine/shell.go
@@ -0,0 +1,65 @@
+package engine
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+)
+
+type ShellEngine struct {}
+
+func NewShellEngine() *ShellEngine {
+	return &ShellEngine{}
+}
+
+func (e *ShellEngine) EnterSandbox(projectRoot string) error {
+	nixPath, err := exec.LookPath("nix")
+	if err != nil {
+		return fmt.Errorf("nix is not installed or not in PATH.\nResolution: Install Nix via 'curl -L https://nixos.org/nix/install | sh'")
+	}
+
+	derrickDir := filepath.Join(projectRoot, ".derrick")
+	if _, err := os.Stat(derrickDir); os.IsNotExist(err) {
+		return fmt.Errorf("sandbox not found at %s.\nResolution: Run 'derrick start' to initialize the environment first", derrickDir)
+	}
+
+	flakePath := fmt.Sprintf("path:%s#default", derrickDir)
+
+	// Define the internal command we want to run INSIDE the nix environment
+	// We use --norc to keep it clean, and --login to ensure a fresh session
+	// \e[34m is Blue, \e[0m is reset
+	customPrompt := "export PS1='\\e[34m(derrick-sandbox)\\e[0m \\w > '; bash --norc"
+
+	// Construct the final Nix command
+	// We use 'sh -c' so that the semicolon is interpreted by a shel
+	cmd := exec.Command(nixPath, "develop", flakePath, "-c", "sh", "-c", customPrompt)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	sigChan := make(chan os.Signal, 1)
+
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGWINCH)
+	defer signal.Stop(sigChan)
+
+	go func() {
+		for sig := range sigChan {
+			if cmd.Process != nil {
+				_ = cmd.Process.Signal(sig)
+			}
+		}
+	}()
+
+	if err := cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return fmt.Errorf("shell existed with status: %d", exitError.ExitCode())
+		}
+		return fmt.Errorf("failed to start sandbox shell: %w", err)
+	}
+
+	return nil
+}
