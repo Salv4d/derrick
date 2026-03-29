70ab54ba1f1c38759ff7143b1101bad99db21622
Salvador Oliveira <slsalvadorlucas@gmail.com>
2026-03-29 03:38:30 -0400

    feat: add stop command

---
+++ internal/cli/stop.go
@@ -0,0 +1,36 @@
+package cli
+
+import (
+	"github.com/Salv4d/derrick/internal/config"
+	"github.com/Salv4d/derrick/internal/engine"
+	"github.com/Salv4d/derrick/internal/ui"
+	"github.com/spf13/cobra"
+)
+
+var stopCmd = &cobra.Command{
+	Use: "stop",
+	Short:	"Stops the local development environment",
+	Long: `Stops all running containers, closes Nix shells, and executes 
+any defined post_stop lifecycle hooks to clean up the environment.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ui.PrintHeader()
+		ui.Info("Stopping Derrick orchestration...")
+
+		filename := "derrick.yaml"
+
+		cfg, err := config.ParseConfig(filename)
+		if err != nil {
+			ui.FailFast(err)
+		}
+
+		ui.Info("⚙️  [Mock] Stopping Docker containers and Nix environments...\n")
+
+		engine.ExecuteHook("post_stop", cfg.Hooks.PostStop)
+
+		ui.Success("Environment successfully stopped!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stopCmd)
+}
