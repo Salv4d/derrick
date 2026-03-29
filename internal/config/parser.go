f5db20aec4bdbd4eb4aacd530c6b5075bc1c4b2e
Salvador Oliveira <slsalvadorlucas@gmail.com>
2026-03-29 02:35:37 -0400

    feat: update parser to improve error visualization on derrick.yaml

--- internal/config/parser.go
+++ internal/config/parser.go
@@ -3,6 +3,9 @@ package config
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -17,8 +20,50 @@ func ParseConfig(filename string) (*ProjectConfig, error) {
 
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse YAML: %w", err)
+		return nil, enhanceYAMLError(data, err)
 	}
 
 	return &config, nil
+}
+
+func enhanceYAMLError(fileData []byte, originalErr error) error {
+	errMsg := originalErr.Error()
+
+	re := regexp.MustCompile(`line (\d+)`)
+	matches := re.FindStringSubmatch(errMsg)
+
+	if len(matches) < 2 {
+		return originalErr
+	}
+
+	lineNum, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return originalErr
+	}
+
+	lines := strings.Split(string(fileData), "\n")
+
+	if lineNum < 1 || lineNum > len(lines) {
+		return originalErr
+	}
+
+	errorLine := lines[lineNum-1]
+
+	trimmedLine := strings.TrimSpace(errorLine)
+	indentLength := len(errorLine) - len(trimmedLine)
+	indicator := strings.Repeat(" ", indentLength) + strings.Repeat("^", len(trimmedLine))
+
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf("Syntax error in derrick.yaml at line %d: \n\n", lineNum))
+
+	if lineNum > 1 {
+		builder.WriteString(fmt.Sprintf("  %3d | %s\n", lineNum-1, lines[lineNum-2]))
+	}
+
+	builder.WriteString(fmt.Sprintf("  %3d | %s\n", lineNum, errorLine))
+	builder.WriteString(fmt.Sprintf("       %s\n\n", indicator))
+
+	builder.WriteString(fmt.Sprintf("Detail: %s", errMsg))
+
+	return fmt.Errorf(builder.String())
 }
