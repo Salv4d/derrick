ab4d520bee8b8b921a27c43d6bdf77ad10daa2da
Salvador Oliveira <slsalvadorlucas@gmail.com>
2026-03-29 20:10:18 -0400

    feat(nix): add "Time Machine" historical package index

---
+++ internal/engine/historical_index.go
@@ -0,0 +1,13 @@
+package engine
+
+var LegacyRegistryMap = map[string]string{
+	"python36":    "github:NixOS/nixpkgs/nixos-19.09",
+	"python37":    "github:NixOS/nixpkgs/nixos-20.03",
+	"python38":    "github:NixOS/nixpkgs/nixos-21.05",
+	"python39":    "github:NixOS/nixpkgs/nixos-21.11",
+	"python310":   "github:NixOS/nixpkgs/nixos-22.11",
+	"nodejs-10_x": "github:NixOS/nixpkgs/nixos-19.09",
+	"nodejs-12_x": "github:NixOS/nixpkgs/nixos-21.05",
+	"nodejs-14_x": "github:NixOS/nixpkgs/nixos-22.05",
+	"nodejs-16_x": "github:NixOS/nixpkgs/nixos-23.05",
+}
