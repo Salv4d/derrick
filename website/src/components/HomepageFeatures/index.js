9327d451abff1076802d541427f432515c4f35ff
Salvador Oliveira <slsalvadorlucas@gmail.com>
2026-04-03 03:25:24 -0400

    docs(ui): replace generic docusaurus landing page with derrick native features

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -4,32 +4,31 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Zero Host Pollution',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Say goodbye to global installations of <code>node</code>, <code>pyenv</code>, or complex versions.
+        Everything runs locked inside an ephemeral Nix sandbox.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Declarative Sandboxes',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Define exactly what your microservices require on a single <code>derrick.yaml</code> contract.
+        From Docker images to Nix packages.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Fail-Fast Validation',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Catch missing secrets, API ports explicitly trapped in use, and missing dependencies in milliseconds before your code even starts.
       </>
     ),
   },
